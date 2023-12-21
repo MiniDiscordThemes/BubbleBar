fa89c3b02319f0dc36fb5db1d726c7f1563d3d98
Saltssaumure <themes@saltssaumure.net>
2023-12-21 03:09:06 +0000

    Rename main.scss -> bubble.scss

--- scss-compile.config.js
+++ scss-compile.config.js
@@ -2,7 +2,7 @@
 export default {
     meta: {
         name: "Bubble Bar" /* The official name of your theme. */,
-        scss: "main" /* The name of your base scss file. */,
+        scss: "bubble" /* The name of your base scss file. */,
         repo: "BubbleBar" /* The name of your theme repository. */,
         version: "dev" /* The version of your theme. */
     }
